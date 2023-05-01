457a325c4bdafaec8e428e1212ae1b344a30eb2c
DeokJong <novelss444@jbnu.ac.kr>
2023-05-01 15:29:58 +0900

    example8

--- src/Chapter8/example8_2_Test_1.java
+++ src/Chapter8/example8_2_Test_1.java
@@ -1,2 +1,25 @@
-package Chapter8;public class example8_2_Test_1 {
+package Chapter8;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+
+public class example8_2_Test_1 {
+    public static void main(String[] args){
+        InputStreamReader in;
+        OutputStreamWriter out;
+        int c;
+
+        try{
+            in=new InputStreamReader(System.in,"US-ASCII");
+            out = new OutputStreamWriter(System.out,"US-ASCII");
+            while((c=in.read())!=-1){
+               out.write(c);
+            }
+        }
+        catch(IOException e){
+            System.out.print("Inout Stream Error!");
+        }
+    }
 }
